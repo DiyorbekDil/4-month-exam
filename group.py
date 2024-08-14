8c3f10a14767cf009d3263c995e25bfef44e01e5
DiyorbekDil <isoqovdiyorbek742@gmail.com>
2024-08-14 09:16:30 +0500

    comlpleted group.py

--- group.py
+++ group.py
@@ -4,8 +4,10 @@ from jsonManager import group_manager, user_manager
 
 
 class Group:
-    def __init__(self, name):
+    def __init__(self, name, start, end):
         self.name = name
+        self.start = start
+        self.end = end
         self.iden_num = randint(0, 1_000_000)
         self.students = []
 
@@ -13,57 +15,63 @@ class Group:
 @log_decorator
 def create_group():
     name = input('Enter group name: ')
-    group = Group(name)
+    start = input('Start time (dd/mm/yyyy): ')
+    end = input('End time (dd/mm/yyyy): ')
+    group = Group(name, start, end)
     group_manager.add(group.__dict__)
     print('Successfully created')
 
 
 @log_decorator
 def add_student_group():
-    group = input('Enter group name: ')
-    all_groups = group_manager.read()
-    i = 0
-    while i < len(all_groups):
-        if all_groups[i]['name'] == group:
-            name = input('Student name: ')
-            try:
-                phone = int(input("Phone: "))
-            except ValueError:
-                print('Phone number must be a whole number!')
-                return add_student_group()
-            all_users = user_manager.read()
-            idx = 0
-            while idx < len(all_users):
-                if all_users[idx]['name'] == name and all_users[idx]['phone'] == phone and\
-                                                all_users[idx]['kind'] == 'student':
-                    student = {'name': name, 'phone': phone}
-                    all_groups[i]['students'].append(student)
-                    group_manager.write(all_groups)
-                    print('Added')
+    try:
+        group = int(input('Enter identity number: '))
+        all_groups = group_manager.read()
+        i = 0
+        while i < len(all_groups):
+            if all_groups[i]['iden_num'] == group:
+                name = input('Student name: ')
+                email = input('Student email: ')
+                all_users = user_manager.read()
+                idx = 0
+                while idx < len(all_users):
+                    if all_users[idx]['name'] == name and all_users[idx]['email'] == email and\
+                                                    all_users[idx]['kind'] == 'student':
+                        student = {'name': name, 'email': email}
+                        all_groups[i]['students'].append(student)
+                        group_manager.write(all_groups)
+                        print('Added')
+                        return
+                    idx += 1
+                else:
+                    print('No such a student')
                     return
-                idx += 1
-            else:
-                print('No such a student')
-                return
-        i += 1
-    else:
-        print('No such a group')
+            i += 1
+        else:
+            print('No such a group')
+    except ValueError:
+        print('Identity number must be a number!')
+        return add_student_group()
 
 
 @log_decorator
 def del_group():
-    group = input('Enter group name: ')
-    all_groups = group_manager.read()
-    i = 0
-    while i < len(all_groups):
-        if all_groups[i]['name'] == group:
-            del all_groups[i]
-            group_manager.write(all_groups)
-            print('Deleted')
-            return
-        i += 1
-    else:
-        print('No such a group')
+    try:
+        group = int(input('Enter identity number: '))
+        all_groups = group_manager.read()
+        i = 0
+        while i < len(all_groups):
+            if all_groups[i]['iden_num'] == group:
+                del all_groups[i]
+                group_manager.write(all_groups)
+                print('Deleted')
+                return
+            i += 1
+        else:
+            print('No such a group')
+    except ValueError:
+        print('Identity number must be a number!')
+        return del_group()
 
 
 @log_decorator
@@ -75,10 +83,10 @@ def show_groups():
 
 
 @log_decorator
-def find_group(name):
+def find_group(iden_num):
     all_groups = group_manager.read()
     for group in all_groups:
-        if group['name'] == name:
+        if group['iden_num'] == iden_num:
             return True
     else:
         return False
